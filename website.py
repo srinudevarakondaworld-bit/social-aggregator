4179db6f9db5ab5fe2cf78e2d9d194309cfc5781
srinudevarakondaworld-bit <srinudevarakondaworld@gmail.com>
2026-07-05 11:39:20 +0530

    Update website.py

--- website.py
+++ website.py
@@ -1,14 +1,9 @@
-import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# నేరుగా st.secrets నుండి డేటా తీసుకోవచ్చు
-if not firebase_admin._apps:
+# website.py లో
     cred = credentials.Certificate({
         "type": st.secrets["type"],
         "project_id": st.secrets["project_id"],
         "private_key_id": st.secrets["private_key_id"],
-        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "private_key": st.secrets["private_key"], # ఇక్కడ .replace అవసరం లేదు
         "client_email": st.secrets["client_email"],
         "client_id": st.secrets["client_id"],
         "auth_uri": st.secrets["auth_uri"],
@@ -16,9 +11,3 @@ if not firebase_admin._apps:
         "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
         "client_x509_cert_url": st.secrets["client_x509_cert_url"]
     })
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://social-aggregator-automation-default-rtdb.firebaseio.com/'
-    })
-
-db = firestore.client()
-st.title("ప్రజా వార్తలు")
