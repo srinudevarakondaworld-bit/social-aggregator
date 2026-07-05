ac6429225e487d61439d8dc1ec18d467c784adb8
srinudevarakondaworld-bit <srinudevarakondaworld@gmail.com>
2026-07-05 10:23:20 +0530

    Update website.py

--- website.py
+++ website.py
@@ -1,27 +1,28 @@
 import streamlit as st
+import json
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials, firestore
 
 # Firebase ఇనిషియలైజేషన్
 if not firebase_admin._apps:
-    cred = credentials.Certificate("social-aggregator-automation-firebase-adminsdk-fbsvc-1b90a4a37d.json")
+    key_dict = json.loads(st.secrets["FIREBASE_JSON"])
+    cred = credentials.Certificate(key_dict)
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://social-aggregator-automation-default-rtdb.firebaseio.com/'
     })
 
+db = firestore.client()
+
 st.set_page_config(page_title="ప్రజా వార్తలు", layout="wide")
-st.title("🌍 లేటెస్ట్ అప్‌డేట్స్")
+st.title("ప్రజా వార్తలు")
 st.markdown("---")
 
-# అప్రూవ్ అయిన వార్తలను 'published_news' నుండి తీసుకుంటుంది
 ref = db.reference('published_news')
 published_news = ref.get()
 
 if published_news:
-    # వార్తలను రివర్స్ ఆర్డర్‌లో చూపిస్తుంది (కొత్తవి పైన వస్తాయి)
     news_items = list(published_news.values())[::-1]
     for news in news_items:
-        # వార్త టైటిల్‌ను చూపిస్తుంది
         st.subheader(news.get('title', 'శీర్షిక లేదు'))
         st.write("---")
 else:
